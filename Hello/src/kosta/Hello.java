d26d4dbce6ba537eb8d5a2969b69056716edcd55
dolsam75 <dolsam77@naver.com>
2019-01-09 11:19:29 +0900

    7

--- Hello/src/kosta/Hello.java
+++ Hello/src/kosta/Hello.java
@@ -6,11 +6,9 @@ public class Hello {
 		System.out.println("hello");
 		System.out.println("HELLO");
 		
-<<<<<<< HEAD
 		System.out.println("master");
-=======
+		
 		System.out.println("sub");
->>>>>>> branch 'sub' of https://github.com/dolsam75/kosta192.git
 	}
 
 }
